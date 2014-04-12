113520b91d84cbec4f69fe84e5eed7dbd5cec294
ramnode <i@minganci.org>
2014-04-13 00:36:18 +0800

    access_token and certifycation

--- handlers/wechat.py
+++ handlers/wechat.py
@@ -3,9 +3,15 @@
 
 import tornado.web
 
+from scripts.checksignature import *
 class wechat(tornado.web.RequestHandler):
     def get(self):
-        self.write('hello world !')
+        signature = self.get_argument('signature')
+        timestamp = self.get_argument('timestamp')
+        nonce = self.get_argument('nonce')
+        echostr = self.get_argument('echostr')
+        if checksig(signature, timestamp, nonce):
+            self.write(echostr)
 
 
 
